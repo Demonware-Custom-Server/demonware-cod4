9768b8d2397b6fd7c63010ab2be6115203264b90
Dylan Ogle <delxhq0@gmail.com>
2021-03-30 00:40:19 +0100

    yes

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,19 @@ import net, { Server } from 'net'
 import Logger from '@bwatton/logger'
 import BinaryStream from './utils/BinaryStream'
 
-const socket: Server = net.createServer()
+const server: Server = net.createServer()
 const logger: Logger = new Logger('Server')
 
-socket.on('data', async (message: Buffer) => {
-  const stream = new BinaryStream(message)
+server.on('connection', socket => {
+  socket.on('data', async (message: Buffer) => {
+    const stream = new BinaryStream(message)
+    const packetId: number = stream.buffer[0]
 
-  const packetId: number = stream.buffer[0]
-})
+    logger.debug(`Got packet: ${packetId}`)
 
-socket.on('connection', socket => {
+    console.log(stream.buffer)
+  })
   logger.debug(`Got connection from ${socket.remoteAddress}:${socket.remotePort}`)
 })
 
-socket.listen(3074, () => logger.debug('Demonware socket running on 3074'))
+server.listen(3074, () => logger.debug('Demonware socket running on 3074'))
