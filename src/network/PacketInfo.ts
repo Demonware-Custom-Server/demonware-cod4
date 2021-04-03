e6a546834cc13d0490e1ec2ac9a118f28cd6e090
Dylan Ogle <delxhq0@gmail.com>
2021-04-03 17:37:50 +0100

    Try and read some packet data. (I have no idea what I'm doing)
    I'm most certain that reads are completely wrong.

---
+++ src/network/PacketInfo.ts
@@ -0,0 +1,3 @@
+export enum PacketInfo {
+  CLIENT_AUTH_TICKET = 0x08,
+}
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,41 @@
 import net, { Server } from 'net'
 import Logger from '@bwatton/logger'
-import BinaryStream from './utils/BinaryStream'
+import { BitStream } from 'bit-buffer'
+import { PacketInfo } from './network/PacketInfo'
+
+const hostAddr = '0.0.0.0'
+const hostPort = 3074
 
 const server: Server = net.createServer()
 const logger: Logger = new Logger('Server')
 
 server.on('connection', socket => {
   socket.on('data', async (message: Buffer) => {
-    const stream = new BinaryStream(message)
-    const packetId: number = stream.buffer[0]
+    const stream = new BitStream(message)
+    const packetID = stream.readInt8()
 
-    logger.debug(`Got packet: ${packetId}`)
+    logger.debug(`Got packetID ${packetID}`)
 
-    console.log(stream.buffer)
+    switch (packetID) {
+      case PacketInfo.CLIENT_AUTH_TICKET:
+        handleClientAuth(stream)
+        break
+      default:
+        logger.error(`Unhandled packet ${packetID}`)
+     }
   })
   logger.debug(`Got connection from ${socket.remoteAddress}:${socket.remotePort}`)
 })
 
-server.listen(3074, () => logger.debug('Demonware socket running on 3074'))
+function handleClientAuth(stream: BitStream) {
+  stream.index = 1
+  const randomBoolean = stream.readBoolean()
+  const randomNumber = stream.readUint32()
+  const gameId = stream.readUint32()
+  const ticketLength = stream.readUint32()
+  // const ticket = stream.readBits(ticketLength)
+
+  logger.debug(`\nRandom Boolean ${randomBoolean}\nRandom Number ${randomNumber}\nGameID ${gameId}\nTicket Len ${ticketLength}\nTicket $`)
+}
+
+server.listen(hostPort, () => logger.debug(`Demonware socket running on ${hostAddr}:${hostPort}`))
